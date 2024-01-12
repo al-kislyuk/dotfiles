b5df6a196efcc20c9752938b9b306f2baf5df0c1
Alexander Kislyuk <al.kislyuk@gmail.com>
2024-01-12 20:00:56 +0200

    add dotfiles set up script

---
+++ setup.sh
@@ -0,0 +1,34 @@
+shopt -s dotglob # too see hidden files and dirs over iteration
+
+confirm() {
+    read -rp "$1? [y/n] " response
+    case "$response" in
+        [yY][eE][sS]|[yY])
+            true
+            ;;
+        *)
+            false
+            ;;
+    esac
+}
+
+for file in .[!.]* # TODO: maybe add ""?
+do
+    if [[ "$file" = ".git" || "$file" = ".gitmodules" ]]; then
+        continue
+    fi
+
+    original_file="$HOME/$file"
+    new_file="$PWD/$file"
+    create_symlink=true
+    if [ -e "$original_file" ]; then
+        if confirm "Do you want to replace $original_file file with $new_file?"; then 
+            rm -r "$original_file" 
+        else
+            create_symlink=false
+        fi
+    fi
+    if $create_symlink; then
+        ln -s "$new_file" "$original_file" 
+    fi
+done 
